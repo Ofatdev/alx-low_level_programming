f380d3c228033b3c72fb583d6c802e0f9e716d20
Ofatdev <fatokun.emma@gmail.com>
2021-06-17 13:56:16 -0700

    C program that prints the size of various types on the computer in which it is complied

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-printf("%lu\n", sizeof(char)); 
-printf("%lu\n", sizeof(int)); 
-printf("%lu\n", sizeof(float)); 
-printf("%lu", sizeof(double)); 
-return 0; 
+printf("%lu\n", sizeof(char));
+printf("%lu\n", sizeof(int));
+printf("%lu\n", sizeof(float));
+printf("%lu", sizeof(double));
+ return (0);
 }
